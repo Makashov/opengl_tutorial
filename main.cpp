0cc165665fa817e0260635c306672d5d917e6dbb
Makashov Nurbol <nm@paybox.money>
2019-12-09 16:06:37 +0600

    Fix glewInit

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 int main(int argc, char * argv[]) {
 
+    const GLint w = 1024, h = 768;
+
     if (!glfwInit()) {
         exit(EXIT_FAILURE);
     }
 
-    const GLint w = 1024, h = 768;
     GLFWwindow * window;
-    window = glfwCreateWindow(w, h, "glfw", nullptr, nullptr);
+    window = glfwCreateWindow(w, h, "Title", nullptr, nullptr);
 
     if (!window) {
         glfwTerminate();
@@ -22,12 +23,31 @@ int main(int argc, char * argv[]) {
     }
     glfwMakeContextCurrent(window);
 
-    const GLubyte * p = glGetString(GL_VERSION);
+    glewExperimental = true;
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(glewError));
+        return EXIT_FAILURE;
+    }
+
+    if (!GLEW_VERSION_3_0) {
+        return EXIT_FAILURE;
+    }
+
+    const GLubyte * p = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    cout << "Graphics driver version: " << p << endl;
+
+    const GLubyte * glsl = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    cout << "GLSL version: " << glsl << endl;
+
+    if (GLEW_ARB_vertex_array_object) {
+        printf("OK");
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glViewport(0, 0, w, h);
-        glClearColor(0, 1, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
